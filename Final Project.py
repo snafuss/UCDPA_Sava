73126b769f87334ccd0499953a8d30c313f3422a
Smaranda <smaranda.sava@gmail.com>
2021-01-23 22:04:07 +0000

    Final project

--- Final Project.py
+++ Final Project.py
@@ -1,5 +1,6 @@
 # importing the Kaggle API
 # pip install kaggle
+import os
 import pandas as pd
 import numpy as np
 
@@ -9,10 +10,9 @@ vaccinesdf = pd.read_csv(file)
 #print(vaccinesdf.shape)
 #print(vaccinesdf)
 
-#
 file2 = "population_by_country_2020.csv"
 popdf = pd.read_csv(file2)
-print(popdf)
+#print(popdf)
 
 # sort country by ppl_fully_vacs. replace missing values w/ 0
 vaccinesdf[["people_fully_vaccinated"]] = vaccinesdf[["people_fully_vaccinated"]].fillna(0)
@@ -29,6 +29,9 @@ total_vac_per_country = vaccinesdf.loc[vaccinesdf.groupby('country').date.idxmax
 total_vac_per_country = total_vac_per_country.sort_values(by='people_fully_vaccinated', ascending=False)
 #print(total_vac_per_country[["country", "people_fully_vaccinated"]])
 
+#merge vaccine.csv with first two columns of popdf.csv
+vacc_tot_pop = pd.merge(total_vac_per_country, popdf.loc[["country","population"]], on='country')
+
 # filter for countries where number of vaccinations is more than 200000
 vaccine_gt_200k = total_vac_per_country[total_vac_per_country.people_fully_vaccinated > 200000][
     ["country", "people_fully_vaccinated"]]
@@ -38,6 +41,9 @@ for index, row in vaccine_gt_200k.iterrows():
     print(str(row[1]) + " people have been fully vaccinated in " + row[0])
 
 
+# sort largest to smallest value (popdf)
+popdf = popdf.sort_values(by='Population', ascending=False)
+print(popdf[["Country","Population"]])
 
 # daily vacs per 1mil country pop
 #vaccinesdf["daily_vaccinations_per_million"] = 1000000 * vaccinesdf[["people_fully_vaccinated"]"] / homelessness["state_pop"]
